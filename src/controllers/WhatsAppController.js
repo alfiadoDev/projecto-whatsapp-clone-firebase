7ce375c0c8af7810f8f8b9ad1ce5728bf6864935
Alfiado Ngana <alfiadoprojectos@gmail.com>
2019-08-05 22:25:28 +0200

    eventos para abrir ou fechar os painei editar perfil e adicionar contact

--- src/controllers/WhatsAppController.js
+++ src/controllers/WhatsAppController.js
@@ -6,6 +6,7 @@ class WhatsAppController{
 
         this.elementsPrototypea();
         this.loadElements();
+        this.initEvents();
 
     }
 
@@ -73,6 +74,38 @@ class WhatsAppController{
         Element.prototype.hasClass = function(name){
             return this.classList.contains(name);
         }
+    }
+
+    initEvents(){
+        
+        this.el.myPhoto.on('click', e=>{
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open');
+            },300);
+        });
+
+        this.el.btnNewContact.on('click', e=>{
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open');
+            },300)
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+            this.el.panelEditProfile.removeClass('open');
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e=>{
+            this.el.panelAddContact.removeClass('open');
+        });
+    }
+
+    closeAllLeftPanel(){
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
     }
 
 }
